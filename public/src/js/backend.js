f5abbdb1c6dd716ff0fffc3e1e2ef461df6768f8
MarWachine <marvin-wachtling@gmx.de>
2023-01-23 15:38:44 +0100

    v0.1.4 BE drawing functionality + image upload

--- public/src/js/backend.js
+++ public/src/js/backend.js
@@ -32,41 +32,103 @@
 'use strict';
 
 import dom from './dom.js';
-import {elements} from './settings.js';
+import { elements } from './settings.js';
 // import { rnd } from './helpers.js';
 
 
 // CONSTANTS / VARIABLES
-let color = 'rgba(0, 0, 0, 0)';
-let size = 5;
+let size = 10;
+let color = 'rgb(0, 0, 0)';
 let lastPos = false;
 let pressed = false;
 
 
 // BASIC
 const domMapping = () => {
+    elements.activeColor = dom.$('.activeColor');
+    elements.fileUpload = dom.$('#fileUpload');
     elements.canvasBE = dom.$('#cImgLoad');
     elements.inputColor = dom.$$('.inputColor');
     elements.inputSize = dom.$('#inputSize');
-    console.log(elements);
+    elements.pColor = dom.$('#pColor');
+    elements.pSize = dom.$('#pSize');
 }
 
 const appendEventlisteners = () => {
     elements.canvasBE.addEventListener('mousemove', drawArea);
     elements.canvasBE.addEventListener('mousedown', btnDown);
     elements.canvasBE.addEventListener('mouseup', btnUp);
-    elements.inputColor.addEventListener('click', setBrushColor);
+    for (let inputC of elements.inputColor) inputC.addEventListener('click', setBrushColor);
     elements.inputSize.addEventListener('change', setBrushSize);
+    elements.fileUpload.addEventListener('submit', handleSubmit);
 }
 
 
 // CONTROLS
-
 const btnDown = () => pressed = true;
 const btnUp = () => pressed = false;
 
 
-// CORE
+// DATA UPLOAD
+const handleSubmit = evt => {
+
+    evt.preventDefault();
+
+    // Transporting formular data via POST
+    fetch('/upload_data', {
+        method: 'post',
+        body: new FormData(elements.fileUpload)     // Generating and sending data with FormData constructor
+    }).then(
+        res => res.text()
+    ).then(
+        renderImgUpload
+    ).catch(
+        console.warn
+    )
+}
+
+const renderImgUpload = img => {
+    const c = elements.canvasBE;
+    const ctx = c.getContext('2d');
+    ctx.clearRect(0, 0, c.width, c.height);
+
+    // Load image from "uploads" folder
+    const thisImg = document.createElement('img');
+    thisImg.src = '/uploads/' + img;
+
+    console.log(thisImg);
+    thisImg.addEventListener('load', () => {
+        // Portrait mode
+        if (thisImg.width <= thisImg.height) {
+            let newImgWidth = thisImg.width * (c.height / thisImg.height);
+            let marginX = (c.width - newImgWidth) / 2;
+            ctx.drawImage(
+                thisImg,
+                marginX,
+                0,
+                newImgWidth,
+                c.height
+            )
+        }
+
+        // Landscape mode
+        else {
+            let newImgHeight = thisImg.height * (c.width / thisImg.width);
+            let marginY = (c.height - newImgHeight) / 2;
+            ctx.drawImage(
+                thisImg,
+                0,
+                marginY,
+                c.width,
+                newImgHeight
+            )
+        }
+    })
+
+}
+
+
+// BACK-END FUNCTIONS
 const initCanvasBE = () => {
     const c = elements.canvasBE;
     // Declare canvas width / height                                                    <-- Optional: Develop responsive sizing solution
@@ -74,40 +136,54 @@ const initCanvasBE = () => {
     c.height = 450;
 }
 
-
-// BACK-END FUNCTIONS
-
 const setBrushColor = evt => {
-    color = evt.target.value;
+    color = window.getComputedStyle(evt.target, null).getPropertyValue('background-color');
+    elements.activeColor.classList.remove('activeColor');
+    evt.target.classList.add('activeColor');
+    elements.activeColor = dom.$('.activeColor');
+    printBrushColor();
 }
 
 const setBrushSize = evt => {
     size = Number(evt.target.value);
+    printBrushSize();
 }
 
 const drawArea = evt => {
     const c = elements.canvasBE;
     const ctx = c.getContext('2d');
 
-    ctx.strokeStyle = color;
+    ctx.fillStyle = `${color}`;
     ctx.lineWidth = size;
-   
+
     if (lastPos && pressed) {
-     ctx.beginPath();
-     ctx.moveTo(lastPos.x, lastPos.y);
-     ctx.lineTo(evt.layerX, evt.layerY);
-     ctx.stroke();
+        ctx.beginPath();
+        ctx.beginPath();
+        ctx.moveTo(lastPos.x, lastPos.y);
+        ctx.lineTo(evt.layerX, evt.layerY);
+        ctx.arc(evt.layerX, evt.layerY, size / 2, 0, 2 * Math.PI);
+        ctx.fill();
     }
 
     lastPos = { x: evt.layerX, y: evt.layerY };
+}
+
+// UI
+const printBrushColor = () => {
+    elements.pColor.innerHTML = `Brush Color: ${color}`;
+}
+
+const printBrushSize = () => {
+    elements.pSize.innerHTML = `Brush Size: ${size}`;
 }
 
 // DCL INIT
 const init = () => {
     domMapping();
     initCanvasBE();
-    render();
     appendEventlisteners();
+    printBrushSize();
+    printBrushColor();
 }
 
 window.addEventListener("load", init);
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 'use strict';
 
-// MODULE
+const formidable = require('formidable');
 const express = require('express');
-
-// SERVER
 const server = express();
 
-// DOCUMENT ROOT
 server.use(express.static('public', {
     extensions: ['html']
 }));
 
-// FUNCTIONS
+// ROUTING
+server.post('/upload_data', (request, response) => {
+    // Prepare data processing
+    const fileUpload = formidable({
+        uploadDir: 'public/uploads',
+        keepExtensions: true,
+    })
+
+    // Process uploaded data
+    fileUpload.parse(request, (err, fields, files) => {
+        if (err) console.warn(err);
+        else {
+            console.log(files);
+        }
+        response.send(files.inputFile.newFilename);
+    })
+})
 
 const init = () => {
     server.listen(80, err => console.log(err || 'Server running'));
